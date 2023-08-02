b4dded004c9679ec31f01ddcba41a01fc5986bb2
Marc Baques Sabat <marcbaquessabat@gmail.com>
2023-08-02 19:11:06 +0200

    Added Premake and new solution

---
+++ premake5.lua
@@ -0,0 +1,103 @@
+workspace "Artifax"
+    architecture "x64"
+
+    configurations
+    { 
+        "Debug", 
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Artifax"
+    location "Artifax"
+    kind "SharedLib"
+    language "C++"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    { 
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        { 
+            "AX_PLATFORM_WINDOWS",
+            "AX_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "AX_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "AX_RELEASE"
+        optimize "On"
+
+    filter "configurations:Debug"
+        defines "AX_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    { 
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Artifax/vendor/spdlog/include",
+        "Artifax/src"
+    }
+
+    links
+    {
+        "Artifax"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        { 
+            "AX_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "AX_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "AX_RELEASE"
+        optimize "On"
+
+    filter "configurations:Debug"
+        defines "AX_DIST"
+        optimize "On"
